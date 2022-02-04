640cec938064ef7ca8a1f1bc80a670f45438635b
Александр Сиденов <sid_san@mail.ru>
2022-02-04 18:37:02 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    int price = 100;           // Стоимость билета
+    int miles = price / 20;    // Количество миль
+        System.out.println("Количество миль = " + miles);
+
+    }
+}
